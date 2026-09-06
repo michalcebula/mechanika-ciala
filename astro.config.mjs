b2f2e9bc9f56c53472398a44a2ce7895287bb50f
michal cebula <michalcebula92@gmail.com>
2026-09-06 18:08:34 +0200

    setup host

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,6 @@
 import { defineConfig } from 'astro/config';
-export default defineConfig({ output: 'static', site: process.env.SITE_URL || undefined, trailingSlash: 'always' });
+
+export default defineConfig({
+  site: 'https://michalcebula.github.io',
+  base: '/mechanika-ciala',
+});
